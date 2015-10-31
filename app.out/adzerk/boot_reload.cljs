e9bb1a8a75d121ca633bed55d5e5e6a52e5b76c6
Jannis Pohlmann <jannis@xfce.org>
2015-10-31 12:15:49 +0100

    Update to c1c4f050271cd738d3f61947f0dd559d1a683d1a

--- app.out/adzerk/boot_reload.cljs
+++ app.out/adzerk/boot_reload.cljs
@@ -1,2 +1,2 @@
 (ns adzerk.boot-reload (:require [adzerk.boot-reload.client :as client] kanban.app))
-(client/connect "ws://localhost:41541" {:on-jsload (fn* [] (kanban.app/run))})
+(client/connect "ws://localhost:47110" {:on-jsload (fn* [] (kanban.app/run))})
